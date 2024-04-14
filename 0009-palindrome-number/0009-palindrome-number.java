87091e85ed4215e4568361b167cdb08d9233bb2b
abelfx <163569205+abelfx@users.noreply.github.com>
2024-04-14 14:29:30 +0300

    Time: 4 ms (100.00%), Space: 43.7 MB (72.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int org_x = x;
+        int reverse = 0;
+        while(x>0){
+            reverse = reverse*10 + x%10;
+            x = x/10;
+        }
+        if (reverse == org_x){
+            return true;
+        }
+        return false;
+    }
+}
