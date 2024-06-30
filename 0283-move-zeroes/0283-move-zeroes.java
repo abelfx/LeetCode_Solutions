b0d706a2c4d386836f57879692980591867a91f1
abelfx <163569205+abelfx@users.noreply.github.com>
2024-06-30 13:09:26 +0300

    Time: 2 ms (83.01%), Space: 46 MB (39.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,27 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        int j = 1;
+        int i =0;
+        int j = 0;
 
-        while(j < nums.length){
-            if(nums[i] == 0 && nums[j] != 0){
+        while(j< nums.length){
+            if(nums[j] != 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                i++; j++;
-            }
-            else if (nums[i] == 0 && nums[j] == 0){
-                j++;
-            }
-            else{
                 i++;
-                j++;
             }
+            j++;
         }
-        
-        System.out.println(Arrays.toString(nums));
-
-
         
     }
 }
