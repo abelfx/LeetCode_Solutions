dfef5cf400402910c680233277a5c1ac5a310fb5
abelfx <163569205+abelfx@users.noreply.github.com>
2024-07-12 18:02:42 +0300

    Time: 60 ms (67.19%), Space: 45.8 MB (62.42%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public String removeStars(String s) {
-         Stack<Character> stack = new Stack();
-         StringBuilder builder = new StringBuilder();
-	     
-		 for(char i: s.toCharArray()) {
-			 if(i != '*') {
-				 stack.push(i);
-			 }
-			 
-		 else if(i == '*') {
-				 stack.pop();
-			 } 
-		 }
-		 
-		 for(char i: stack) {
-            builder.append(i);
-		 }
-
-         return builder.toString();
-		 
+       Stack<Character> stack = new Stack();
+	        StringBuilder builder = new StringBuilder();
+	        
+	        for(char i: s.toCharArray()){
+	            if(i == '*'){
+	                stack.pop();
+	            }else{
+	                stack.push(i);
+	            }
+	        }
+	        
+	        for(char i: stack){
+	            builder.append(i);
+	        }
+	            
+	        return builder.toString();
+            
+            
         
     }
 }
