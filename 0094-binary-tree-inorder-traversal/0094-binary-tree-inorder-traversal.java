d8906113c52ec70bc037c49933d12d544e94a18c
abelfx <163569205+abelfx@users.noreply.github.com>
2024-07-23 11:51:50 +0300

    Time: 0 ms (100.00%), Space: 41.6 MB (37.48%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,16 +15,18 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        
         List<Integer> list = new ArrayList();
-
+        
         dfs(list, root);
-
+        
         return list;
         
     }
-
+    
     private void dfs(List<Integer> list, TreeNode root){
         if(root == null) return;
+        
         dfs(list, root.left);
         list.add(root.val);
         dfs(list, root.right);
