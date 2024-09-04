4ed867d76d0d45cee2370e8aaafa6be7120aa7e1
abelfx <163569205+abelfx@users.noreply.github.com>
2024-09-04 11:31:31 +0300

    Time: 0 ms (100.00%), Space: 42.4 MB (47.32%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,12 +10,10 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        else if(list2 == null) return list1;
-
-        ListNode result = new ListNode(0);
+        
+        ListNode result =  new ListNode(0);
         ListNode pointer = result;
-
+        
         while(list1 != null && list2 != null){
             if(list1.val <= list2.val){
                 pointer.next = list1;
@@ -24,19 +22,17 @@ class Solution {
                 pointer.next = list2;
                 list2 = list2.next;
             }
+            
             pointer = pointer.next;
         }
-
-         if(list1 == null){
-                pointer.next = list2;
-                list2 = list2.next;
-            }
-            else if(list2 == null){
-                pointer.next = list1;
-                list1 = list1.next;
-            }
-
+        
+        if(list1 != null){
+            pointer.next = list1;
+        }else if(list2 != null){
+            pointer.next = list2;
+        }
+        
         return result.next;
-    
+        
     }
 }
