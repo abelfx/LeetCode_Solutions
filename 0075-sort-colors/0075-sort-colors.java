fc1636d2971dbb05a86d4d03dc395b3a88585a36
abelfx <163569205+abelfx@users.noreply.github.com>
2024-12-17 07:24:38 +0300

    Time: 0 ms (100.00%), Space: 42.7 MB (9.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -5,14 +5,63 @@ class Solution {
     }
     
     private void Sort(int[] array){
-        for(int i = 0; i < array.length - 1; i ++){
-            for(int j = 0; j < array.length - 1 - i; j++){
-                if(array[j] > array[j+1]){
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                } 
+        
+        int length = array.length;
+        if(length <= 1){
+return;}
+        int middle = array.length/2;
+        int right = length - middle;
+        
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[right];
+        
+        int i = 0;
+        int j = 0;
+        
+        while( i<length){
+            if(i < leftArray.length){
+                leftArray[i] = array[i];
+            }else {
+                rightArray[j] = array[i];
+                j++;
             }
+            i++; 
         }
+        
+        Sort(leftArray);
+        Sort(rightArray);
+        merger(leftArray, rightArray, array);
     }
+    
+    
+        private void merger(int[] leftArray, int[] rightArray, int[] array){
+          int leftLength = leftArray.length;
+          int rightLength = rightArray.length;
+          
+          int l = 0;
+          int r = 0;
+          int j = 0;
+          while(l < leftLength && r < rightLength){
+              if(leftArray[l] < rightArray[r]){
+                  array[j] = leftArray[l];
+                  l++;
+              }else {
+                  array[j] = rightArray[r];
+                  r++;
+              }
+              j++;
+          }
+            
+          while( l < leftLength){
+              array[j] = leftArray[l];
+              j++;
+              l++;
+          }
+          
+          while( r < rightLength){
+              array[j] = rightArray[r];
+              j++;
+              r++;
+          }
+        }
 }
